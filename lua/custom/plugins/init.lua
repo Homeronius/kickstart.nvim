c2a797447fac5f86a5e85346362e2344a5b9043a
Tobia Clagluena <homeronius8050@gmail.com>
2023-05-01 18:21:19 +0200

    Add custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {'romgrk/barbar.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},
+  {'majutsushi/tagbar'},
+  {'tpope/vim-obsession'},
+  -- Disabled because of mini.animate plugin
+  -- {'psliwka/vim-smoothie'},
+  {'ludovicchabant/vim-gutentags'},
+  {'kdheepak/lazygit.nvim'},
+  {'github/copilot.vim'},
+  -- { "m4xshen/smartcolumn.nvim", opts = {scope = "line"} },
+
+  -- Colorschemes
+  {'ellisonleao/gruvbox.nvim'},
+  {'overcache/NeoSolarized'},
+  {'Th3Whit3Wolf/space-nvim'},
+  {'tanvirtin/monokai.nvim'},
+}
